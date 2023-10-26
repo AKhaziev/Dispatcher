84bb907e45931e95312257b2d90b1f555e04ea96
a.khaziev <alexander.khaziev@yandex.com>
2023-10-26 15:24:58 +0500

    first commit

---
+++ lib/pages/vehicle/vehicle_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class VehiclePage extends StatelessWidget {
+  const VehiclePage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
